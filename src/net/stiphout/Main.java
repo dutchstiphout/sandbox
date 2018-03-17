9f6c8d57773c771f0ce3c1fa1f7dfaaa9acdd930
Dutch Stiphout <dutch@stiphout.net>
2018-03-17 16:49:10 -0700

    Testing minor update to Main.java to see if git integration is working.

--- src/net/stiphout/Main.java
+++ src/net/stiphout/Main.java
@@ -2,6 +2,15 @@ package net.stiphout;
 
 public class Main {
   public static void main(String[] args) {
-	  System.out.println("Something new...");
+
+    assert mixStart("mix snacks");
+    assert mixStart("pix snacks");
+    assert !mixStart("piz snacks");
+
   }
+  private static boolean mixStart(String str) {
+    return (str.substring(0,2).matches(".ix"));
+  }
+
+
 }
