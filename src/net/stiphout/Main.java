3b38e6153e6bc5c86cb2968041f8f7a6c5932de0
Dutch Stiphout <dutch@stiphout.net>
2018-03-17 16:36:21 -0700

    Testing minor update to Main.java to see if git integration is working.

--- src/net/stiphout/Main.java
+++ src/net/stiphout/Main.java
@@ -2,6 +2,6 @@ package net.stiphout;
 
 public class Main {
   public static void main(String[] args) {
-	  System.out.println("Something else...");
+	  System.out.println("Something new...");
   }
 }
