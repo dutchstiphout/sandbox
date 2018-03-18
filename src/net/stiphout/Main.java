2ac0e3d864a7ac1975f9b514608e44038f2b584b
Dutch Stiphout <dutch@stiphout.net>
2018-03-17 18:05:43 -0700

    Fun with codingbat 2

--- src/net/stiphout/Main.java
+++ src/net/stiphout/Main.java
@@ -3,16 +3,20 @@ package net.stiphout;
 public class Main {
   public static void main(String[] args) {
 
-    if (worker("Chocolate",2).equals("ChoCho")) { System.out.println("Check!"); } else { System.out.println("Fuck!"); }
-    if (worker("Chocolate",3).equals("ChoChoCho")) { System.out.println("Check!"); } else { System.out.println("Fuck!"); }
-    if (worker("abc",3).equals("abcabcabc")) { System.out.println("Check!"); } else { System.out.println("Fuck!"); }
-    if (worker("",3).equals("")) { System.out.println("Check!"); } else { System.out.println("Fuck!"); }
+    if (worker("abcxx")==1) { System.out.println("Check!"); } else { System.out.println("Fuck!"); }
+    if (worker("xxx")==2) { System.out.println("Check!"); } else { System.out.println("Fuck!"); }
+    if (worker("xxxx")==3) { System.out.println("Check!"); } else { System.out.println("Fuck!"); }
 
   }
-  private static String worker(String str, int n) {
-    String retVal = "";
-    for (int i=0;i<n;i++) retVal = retVal.concat(str.substring(0, Math.min(3, str.length())));
-    return retVal;
+  private static int worker(String str) {
+    int occurences = 0;
+    char[] ca = str.toCharArray();
+    for (int i = 0; i<ca.length-1;i++) {
+      if (ca[i]=='x' && ca[i+1]=='x') {
+        occurences++;
+      }
+    }
+    return  occurences;
   }
 
 
