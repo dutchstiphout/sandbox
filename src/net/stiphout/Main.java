9369243028013d05dccf4672602fe157340803f7
Dutch Stiphout <dutch@stiphout.net>
2018-03-17 17:50:30 -0700

    Sample problem from codingbat.com

--- src/net/stiphout/Main.java
+++ src/net/stiphout/Main.java
@@ -3,13 +3,16 @@ package net.stiphout;
 public class Main {
   public static void main(String[] args) {
 
-    assert mixStart("mix snacks");
-    assert mixStart("pix snacks");
-    assert !mixStart("piz snacks");
+    if (worker("Chocolate",2).equals("ChoCho")) { System.out.println("Check!"); } else { System.out.println("Fuck!"); }
+    if (worker("Chocolate",3).equals("ChoChoCho")) { System.out.println("Check!"); } else { System.out.println("Fuck!"); }
+    if (worker("abc",3).equals("abcabcabc")) { System.out.println("Check!"); } else { System.out.println("Fuck!"); }
+    if (worker("",3).equals("")) { System.out.println("Check!"); } else { System.out.println("Fuck!"); }
 
   }
-  private static boolean mixStart(String str) {
-    return (str.substring(0,2).matches(".ix"));
+  private static String worker(String str, int n) {
+    String retVal = "";
+    for (int i=0;i<n;i++) retVal = retVal.concat(str.substring(0, Math.min(3, str.length())));
+    return retVal;
   }
 
 
